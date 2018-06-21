29ecad039762d8d349ab84ce69a07f44d6f3e644
sammarcus <sam.marcus@me.com>
2018-06-21 14:58:01 -0400

    added solution

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+function getUKTime() {
+  let UKtime = new Date().getHours() + 5;
+  return UKtime
+}
+
+function discountTicket(originalPrice) {
+  let actualPrice = originalPrice*0.8;
+  return actualPrice
+}
+
+function tweetReply(fanName) {
+  let greeting = `Hey ${fanName}, thanks so much for the support! I really appreciate it!`
+  return greeting
+}
