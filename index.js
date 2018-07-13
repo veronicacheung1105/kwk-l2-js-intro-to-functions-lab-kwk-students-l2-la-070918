59162ebb460d19b3a6d50692ca4b1f59a6de9594
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-13 21:49:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // Write your code in this file!
 
+function getUKTime() {
+  var time = new Date().getHours() + 8 
+  return time
+}
+ 
+ function discountTicket(input) {
+  return input * 0.8;
+}
 
+function tweetReply(name){
+  return `Hey ${name}, thanks so much for the support! I really appreciate it!`
+}
